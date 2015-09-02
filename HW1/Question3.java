d544daa5e5bd782a54397bf91871f8a7e26b6c64
Hanyao <nbweige@gmail.com>
2015-09-02 19:30:49 -0400

    HW1

---
+++ HW1/Question3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MyTemp{
+	public static void main(String[] args){
+		double m = 12 * Math.random();
+		
+		final String[] month = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+		
+		System.out.println(month[(int)m]);
+		
+	}
+}
